556d62e527659d469a71426a31f5f435b6ad43e5
loganhartford <logan.hartford@outlook.com>
2024-12-16 09:33:48 -0500

    custom tokenizer for echo behavior

--- shell.c
+++ shell.c
@@ -6,34 +6,67 @@
 #define MAX_INPUT_LENGTH 1000
 #define MAX_TOKENS 100
 
-void trim_whitespace(char *str)
+char *custom_strtok(char *str, const char *delim, char **saveptr)
 {
-    char *end;
-    char *start = str;
+    char *token;
+    int in_quotes = 0;
+    char quote_char = '\0';
 
-    while (*start == ' ')
+    /* If str is NULL, start from where we left off */
+    if (str == NULL)
     {
-        start++;
+        str = *saveptr;
     }
 
-    // Move the start of the trimmed string
-    if (start != str)
+    /* Skip leading delimiters */
+    while (*str && strchr(delim, *str))
     {
-        memmove(str, start, strlen(start) + 1);
+        str++;
     }
 
-    // Empty string
+    /* No more tokens */
     if (*str == '\0')
-        return;
-
-    end = str + strlen(str) - 1;
-    while (end > str && *end == ' ')
     {
-        end--;
+        *saveptr = str;
+        return NULL;
     }
 
-    // Add null terminator
-    *(end + 1) = '\0';
+    token = str;
+
+    /* Find the end of the token */
+    while (*str)
+    {
+        /* Toggle in_quotes if we find a quote character */
+        if (*str == '"' || *str == '\'')
+        {
+            if (in_quotes && *str == quote_char)
+            {
+                in_quotes = 0;
+            }
+            else if (!in_quotes)
+            {
+                in_quotes = 1;
+                quote_char = *str;
+            }
+        }
+        /* If we are not in quotes and we find a delimiter, break */
+        else if (!in_quotes && strchr(delim, *str))
+        {
+            break;
+        }
+        str++;
+    }
+
+    /* Terminate the token */
+    if (*str)
+    {
+        *str = '\0';
+        str++;
+    }
+
+    /* Save the position for the next call */
+    *saveptr = str;
+    return token;
 }
 
 int main(void)
@@ -42,55 +75,52 @@ int main(void)
 
     while (1)
     {
-        // Use stderr for the prompt to keep it separate from command output
+        /* Use stderr for the prompt to keep it separate from command output */
         fprintf(stderr, "$ ");
 
         if (fgets(input, sizeof(input), stdin) == NULL)
         {
-            // EOF received, exit
+            /* EOF received, exit */
             break;
         }
 
-        // Remove the newline character
+        /* Properly terminate the input string */
         input[strcspn(input, "\n")] = '\0';
-
-        trim_whitespace(input);
 
         if (strlen(input) == 0)
         {
             continue;
         }
 
-        printf("Input: %s\n", input);
-
         if (strncmp(input, "exit", 4) == 0)
         {
             break;
         }
 
-        // Tokenize the input
+        /* Tokenize the input */
         char *tokens[MAX_TOKENS];
         int token_count = 0;
-        char *token = strtok(input, " ");
+        char *saveptr;
+        char *token = custom_strtok(input, " ", &saveptr);
         while (token != NULL && token_count < MAX_TOKENS)
         {
             tokens[token_count++] = token;
-            token = strtok(NULL, " ");
+            token = custom_strtok(NULL, " ", &saveptr);
         }
         tokens[token_count] = NULL;
 
-        // If we have tokens, execute the first one
+        /* Execute the command */
         if (token_count > 0)
         {
             pid_t pid = fork();
-            if (pid == 0) // Child
+            if (pid == 0) /* Child */
             {
                 execvp(tokens[0], tokens);
-                // execvp only returns if there is an error
+                /* execvp only returns if there is an error */
                 perror("error");
                 exit(EXIT_FAILURE);
             }
-            else // Parent
+            else /* Parent */
             {
                 wait(NULL);
             }
